6f40373ff803bf5934569870210bdebb47097d92
petraalpigiano <petraalpigiano@gmail.com>
2025-05-23 15:36:26 +0200

    add card css

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,14 @@ export default function App() {
           <button className="button">Express</button>
           <button className="button">ReactJS</button>
         </div>
-        <div className="container-content"></div>
+        <div className="container-card">
+          <div className="card-title">HTML</div>
+          <div className="card-content">
+            HTML (HyperText Markup Language) è il linguaggio standard per creare
+            pagine e applicazioni web. Struttura il contenuto web e fornisce
+            elementi di base come titoli, paragrafi e immagini.
+          </div>
+        </div>
       </div>
     </>
   );
