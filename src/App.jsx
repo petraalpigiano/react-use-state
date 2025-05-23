8d2a60b68ccae6cbcd73445b848e25a0f8ba82ad
petraalpigiano <petraalpigiano@gmail.com>
2025-05-23 15:29:18 +0200

    add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ export default function App() {
       <div className="background-title">
         <h1>Learn Web Development</h1>
       </div>
+      <div className="container ">
+        <div className="container-buttons">
+          <button className="button active">HTML</button>
+          <button className="button">CSS</button>
+          <button className="button">JavaScript</button>
+          <button className="button">Node.js</button>
+          <button className="button">Express</button>
+          <button className="button">ReactJS</button>
+        </div>
+        <div className="container-content"></div>
+      </div>
     </>
   );
 }
